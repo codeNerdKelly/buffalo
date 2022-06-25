4c90db029bdee7e8cdeced253b744f70e1ed94b7
codeNerdKelly <48159112+codeNerdKelly@users.noreply.github.com>
2022-06-25 18:59:02 -0400

    inserting github link

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import "./App.css";
 import Search from "./Search";
 import Description from "./Description";
 import Weather from "./Weather";
+import Github from "./Github";
 
 export default function App() {
   return (
+    <div>
     <div className="App">
       <Search />
       <h1 className="City">New York</h1>
       <Description />
       <Weather />
+    </div>
+    <Github/>
     </div>
   );
 }
---
+++ src/Github.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Github.css";
+export default function Github(){
+
+    return(
+        <div className = "Github">
+            <h4>
+                <a className = "Link" href = "https://github.com/codeNerdKelly/buffalo">created by codeNerdKelly</a>
+            </h4>
+        </div>
+    )
+}
