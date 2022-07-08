7f0286addc56b0f959cea2abaf2307c1c7b6d82f
codeNerdKelly <48159112+codeNerdKelly@users.noreply.github.com>
2022-07-08 08:13:33 -0400

    starting over

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import "./App.css";
-import Weather from "./Weather";
 import Github from "./Github";
 
 export default function App() {
@@ -8,7 +7,7 @@ export default function App() {
     <div className="App">
       <div className = "container">
    
-      <Weather defaultCity = "Vienna"/>
+     
     </div>
     </div>
     <Github/>
